27eb9317b0e0dd800a5790406eb7fbaf32a090f1
Ankit Gupta <ankitgupta17878@gmail.com>
2024-07-27 23:33:03 +0530

    -

--- client/src/app/components/expanseData.jsx
+++ client/src/app/components/expanseData.jsx
@@ -16,8 +16,8 @@ function expanseData() {
       <div className=" flex justify-between px-3 relative"><p>{exFilter + " " + "Expenses"}</p> <button className="text-sm rounded-md border p-0.5 px-2 hover:bg-gray-100 active:bg-gray-200 " onClick={() => setShowFilter((pre) => !pre)}><p>Filter</p></button>
 
         {showFilter && <ul className="absolute right-2 top-7 text-[13px] border p-1 rounded-md ">
-          <li className="border-b py-0.5 hover:bg-gray-100 active:bg-gray-200 cursor-pointer px-2">weekly</li>
-          <li className="border-b  py-0.5 hover:bg-gray-100 active:bg-gray-200 cursor-pointer px-2">Monthly</li>
+          <li className="border-b py-0.5 hover:bg-gray-100 active:bg-gray-200 cursor-pointer px-2"  onClick={()=>setExFilter('weekly')}>weekly</li>
+          <li className="border-b  py-0.5 hover:bg-gray-100 active:bg-gray-200 cursor-pointer px-2" onClick={()=>setExFilter('Monthly')}>Monthly</li>
           <li className=" py-0.5 hover:bg-gray-100 active:bg-gray-200 px-2 cursor-pointer">Yearly</li>
         </ul>}
 
