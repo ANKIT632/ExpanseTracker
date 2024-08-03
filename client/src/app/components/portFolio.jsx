3b09f9b389d8240befa25545f7bd0319435834ec
Ankit Gupta <ankitgupta17878@gmail.com>
2024-08-03 23:32:51 +0530

    -

--- client/src/app/components/portFolio.jsx
+++ client/src/app/components/portFolio.jsx
@@ -4,36 +4,19 @@ import PortfolioLineBar from './portfolioLineBar';
 
 function portFolio() {
 
-    const data = {
-        labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
-        datasets: [
-          {
-            label: 'Dataset 1',
-            data: [65, 59, 80, 81, 56, 55, 40],
-            fill: false,
-            borderColor: 'rgba(75,192,192,1)',
-            tension: 0.1
-          }
-        ]
-      };
+
       
-      const options = {
-        scales: {
-          y: {
-            beginAtZero: true
-          }
-        }
-      };
+ 
 
   return (
-    <div className="flex-1 bg-blue-100 rounded-md ">
-        <div className="text-center"><p>Total balance</p> <p>{4000}</p></div>
+    <div className="flex-1 bg-blue-100 rounded-md p-2">
+        <div className="text-center text-zinc-600 text-md font-semibold"><p>Buget balance</p> <p className='text-blue-500 text-[14px]'>{4000}</p></div>
 
         <div>
             <p className="text-xs font-bold">Expense progress</p>
             <div>
 
-            <PortfolioLineBar data={data} options={options}/>
+
             </div>
 
             <div className="flex justify-center ">
@@ -47,3 +30,4 @@ function portFolio() {
 }
 
 export default portFolio
+
